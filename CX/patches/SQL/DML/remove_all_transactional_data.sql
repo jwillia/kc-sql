481a125e63210dc4ce2a060cb2b55b2431b02bca
Lance Speelmon <lance@speelmon.com>
2014-07-14 13:49:13 -0700

    Do not disable system accounts: kr, admin, notsys, kc

--- CX/patches/SQL/DML/remove_all_transactional_data.sql
+++ CX/patches/SQL/DML/remove_all_transactional_data.sql
@@ -18,11 +18,11 @@ END;
 START TRANSACTION;
 
 -- select * from krim_entity_t;
--- disable all entities except admin
-update krim_entity_emp_info_t set ACTV_IND='N' where ENTITY_ID != '1100';
-update krim_entity_afltn_t set ACTV_IND='N' where ENTITY_ID != '1100';
-update krim_entity_t set ACTV_IND='N' where ENTITY_ID != '1100';
-update krim_prncpl_t set ACTV_IND='N' where ENTITY_ID != '1100';
+-- disable all entities except kr, admin, notsys, kc
+update krim_entity_emp_info_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
+update krim_entity_afltn_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
+update krim_entity_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
+update krim_prncpl_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
 delete from eps_prop_person_ext;
 delete from person_ext_t;
 -- delete all entities except admin
