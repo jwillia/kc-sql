165253319f30a891a5db899f7520812d8b4c4c62
Lance Speelmon <lance@speelmon.com>
2014-10-01 15:15:05 -0700

    KcPerson search results not producing accurate results - Inactive People do not show when Both is selected https://github.com/rSmart/issues/issues/440

--- tools/SQL/remove_all_demo_data.sql
+++ tools/SQL/remove_all_demo_data.sql
@@ -19,17 +19,14 @@ START TRANSACTION;
 
 -- select * from krim_entity_t;
 -- disable all entities except kr, admin, notsys, kc
-update krim_entity_emp_info_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
-update krim_entity_afltn_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
-update krim_entity_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
+
+-- https://github.com/rSmart/issues/issues/440
+-- update krim_entity_emp_info_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
+-- update krim_entity_afltn_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
+-- update krim_entity_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
 update krim_prncpl_t set ACTV_IND='N' where ENTITY_ID NOT IN ('1', '1100', '1131', '1132');
 delete from eps_prop_person_ext;
 delete from person_ext_t;
--- delete all entities except admin
--- delete from krim_entity_emp_info_t where ENTITY_AFLTN_ID in (select ENTITY_AFLTN_ID from krim_entity_afltn_t where ENTITY_ID != '1100');
--- delete from krim_entity_afltn_t where ENTITY_ID != '1100';
--- delete from krim_entity_t where ENTITY_ID != '1100';
--- delete from krim_prncpl_t where ENTITY_ID != '1100';
 
 -- deactivate demo users in KRIM_ROLE_MBR_T and KRIM_GRP_MBR_T
 update KRIM_ROLE_MBR_T SET ACTV_TO_DT = NOW(), LAST_UPDT_DT = NOW() WHERE MBR_TYP_CD = 'P' and MBR_ID IN (
