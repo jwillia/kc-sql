10a61646bbf4b48680b536ee4df9d6ee128627e2
Lance Speelmon <lance@speelmon.com>
2014-11-19 10:41:59 -0700

    SQL files have been moved to: https://github.com/rSmart/puppet/tree/master/modules/cx_data_migration/files

---
+++ tools/SQL/README.md
@@ -0,0 +1,1 @@
+SQL files have been moved to: https://github.com/rSmart/puppet/tree/master/modules/cx_data_migration/files
--- tools/SQL/delete_questionnaires.sql
+++
@@ -1,6 +0,0 @@
--- FIXME should deactivate insead of deleting.
-delete from questionnaire_answer;
-delete from questionnaire_answer_header;
-delete from questionnaire_questions;
-delete from questionnaire_usage;
-delete from questionnaire;
