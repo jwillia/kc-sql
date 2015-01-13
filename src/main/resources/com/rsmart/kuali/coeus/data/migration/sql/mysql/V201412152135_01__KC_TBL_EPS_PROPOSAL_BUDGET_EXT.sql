79c5422bc11df792d5c7c6869cb37ed1d416a9b7
blackcathacker <douglas.pace@gmail.com>
2015-01-12 17:29:04 -0800

    Adding SQL from Kuali Coeus 6.0 See https://github.com/kuali/kc for source of these scripts Adding flyway maven plugin and mysql for manually applying Flyway migrations.

---
+++ src/main/resources/com/rsmart/kuali/coeus/data/migration/sql/mysql/V201412152135_01__KC_TBL_EPS_PROPOSAL_BUDGET_EXT.sql
@@ -0,0 +1,35 @@
+DELIMITER /
+
+alter table EPS_PROPOSAL_BUDGET_EXT drop column VER_NBR
+/
+alter table EPS_PROPOSAL_BUDGET_EXT drop column OBJ_ID
+/
+alter table EPS_PROPOSAL_BUDGET_EXT drop column UPDATE_TIMESTAMP
+/
+alter table EPS_PROPOSAL_BUDGET_EXT drop column UPDATE_USER
+/
+
+alter table EPS_PROPOSAL_BUDGET_EXT add column PROPOSAL_NUMBER varchar(12)
+/
+
+update EPS_PROPOSAL_BUDGET_EXT budget set PROPOSAL_NUMBER = 
+	(select prop.PROPOSAL_NUMBER from EPS_PROPOSAL prop 
+		left join BUDGET_DOCUMENT budgetDoc on prop.DOCUMENT_NUMBER = budgetDoc.PARENT_DOCUMENT_KEY 
+	where budgetDoc.DOCUMENT_NUMBER = budget.DOCUMENT_NUMBER)
+/
+
+alter table EPS_PROPOSAL_BUDGET_EXT modify column PROPOSAL_NUMBER varchar(12) not null
+/
+
+alter table EPS_PROPOSAL_BUDGET_EXT add column STATUS_CODE char(1)
+/
+
+update EPS_PROPOSAL_BUDGET_EXT prop_budget set STATUS_CODE = (select BUDGET_STATUS from EPS_PROPOSAL prop where prop.PROPOSAL_NUMBER = prop_budget.PROPOSAL_NUMBER) where (select FINAL_VERSION_FLAG from BUDGET where prop_budget.BUDGET_ID = BUDGET.BUDGET_ID) = 'Y'
+/
+
+update EPS_PROPOSAL_BUDGET_EXT prop_budget set STATUS_CODE = '2' where STATUS_CODE is null
+/
+
+alter table EPS_PROPOSAL_BUDGET_EXT modify column STATUS_CODE char(1) not null
+
+DELIMITER ;
