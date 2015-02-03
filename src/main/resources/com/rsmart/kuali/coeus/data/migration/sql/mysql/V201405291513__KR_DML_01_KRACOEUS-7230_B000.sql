0e8354c981c85fb6844f4d502122f80f5d5e43f4
blackcathacker <douglas.pace@gmail.com>
2015-02-02 17:46:40 -0800

    Further cleanup again

--- src/main/resources/com/rsmart/kuali/coeus/data/migration/sql/mysql/V201405291513__KR_DML_01_KRACOEUS-7230_B000.sql
+++
@@ -1,7 +0,0 @@
-DELIMITER /
-update krms_term_rslvr_parm_spec_t set nm = 'Questionnaire ID' where nm = 'Questionnaire Ref ID'
-/
-update krms_term_parm_t set nm = 'Questionnaire ID' where nm = 'Questionnaire Ref ID'
-/
-DELIMITER ;
-
